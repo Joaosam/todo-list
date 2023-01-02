70a8022e5e4d4c03d33e065f56c54343b09bf488
root <root@HI-JOAO-MENDES>
2023-01-02 17:42:45 -0300

    Update todo

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,26 +1,32 @@
-import styles from './Task.module.css'
+import styles from "./Task.module.css";
 import { Trash } from "phosphor-react";
-import { useState } from 'react';
+import { useState } from "react";
 
 type TaskProps = {
-  descriptionTask: string,
-  id: string,
-  checkedTask: boolean,
+  descriptionTask: string;
+  id: string;
+  checkedTask: boolean;
   onDeleteTask: (idCurrentTask: string) => void;
-  onCompleteTask: (idCurrentTask: string, isCheckedTask: boolean) => void
-}
+  onCompleteTask: (idCurrentTask: string, isCheckedTask: boolean) => void;
+};
 
-export function Task({ id, descriptionTask, checkedTask, onDeleteTask, onCompleteTask }: TaskProps) {
-  const [check, setCheck] = useState(false)
-  const validateCheckTask = checkedTask === true ? true: false
+export function Task({
+  id,
+  descriptionTask,
+  checkedTask,
+  onDeleteTask,
+  onCompleteTask,
+}: TaskProps) {
+  const [check, setCheck] = useState(false);
+  const validateCheckTask = checkedTask === true ? true : false;
 
   function toggleInput() {
-    setCheck(!check)
-    onCompleteTask(id, !check)
+    setCheck(!check);
+    onCompleteTask(id, !check);
   }
 
   function handleDeleteTask() {
-    onDeleteTask(id)
+    onDeleteTask(id);
   }
 
   return (
@@ -28,23 +34,23 @@ export function Task({ id, descriptionTask, checkedTask, onDeleteTask, onComplet
       <div className={styles.task}>
         <div className={styles.content}>
           <input
-           onClick={toggleInput}
-           id={id}
-           type="checkbox"
-           checked={validateCheckTask}
-           onChange={() => validateCheckTask}
-           />
+            onClick={toggleInput}
+            id={id}
+            type="checkbox"
+            checked={validateCheckTask}
+            onChange={() => validateCheckTask}
+          />
           <label htmlFor={id}></label>
           <div className={styles.descriptionTask}>
-            <p className={checkedTask === true ? styles.checked : undefined}>{descriptionTask}</p>
+            <p className={checkedTask === true ? styles.checked : undefined}>
+              {descriptionTask}
+            </p>
           </div>
-          <button 
-            onClick={handleDeleteTask}
-            title='Apagar Tarefa'>
-            <Trash size={24}/>
+          <button onClick={handleDeleteTask} title="Apagar Tarefa">
+            <Trash size={24} />
           </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
